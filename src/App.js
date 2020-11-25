4c58e70cdb5fafd2a2a65757e6c447d47d83d9ee
chicoumbetov <shyngys-maaan@mail.ru>
2020-11-26 00:10:46 +0600

    Sidebar over Navbar, opacity hide/show by comment sign in SidebarElements/SidebarContainer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
     
         return(
             <Router>
+                <Sidebar/>
                 <Navbar/>
             </Router>
             
