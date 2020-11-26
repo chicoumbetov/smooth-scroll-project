d0db91b75ae0adedf3193f5958d25430da3f4205
chicoumbetov <shyngys-maaan@mail.ru>
2020-11-26 22:02:22 +0600

    Creating Button Elements

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import Video from '../../videos/video1.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+import React, { useState } from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
+import {
+    HeroContainer,
+    HeroBg, VideoBg,
+    HeroContent, HeroH1,
+    HeroP, HeroBtnWrapper,
+    ArrowForward, ArrowRight
+} from './HeroElements';
 
+//1:15:00
 const HeroSection = () => {
+
+    const [ hover , setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+ 
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video1.mp4' />
+                <VideoBg autoPlay loop muted src={Video} type='video.mp4' />
             </HeroBg>
+
+            <HeroContent>
+                <HeroH1>Virtual Banking made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and
+                    receive $250 in credit towards your
+                    next payment.
+                </HeroP>
+                <HeroBtnWrapper>
+
+                    <Button to='signup'
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHover}
+                        primary='true'
+                        dark='true'
+                    >  
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
